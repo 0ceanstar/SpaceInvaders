daece48dafd27ee7ea24d28c50bc824f5064f24c
rfli <rf_li9072@163.com>
2022-04-09 23:58:29 -0700

    delete cheating code to recover a normal player

--- src/main/java/spaceinvaders/client/mvc/GameController.java
+++ src/main/java/spaceinvaders/client/mvc/GameController.java
@@ -203,9 +203,10 @@ public class GameController implements Controller {
       if (event.getKeyCode() == VK_LEFT) {
         if (model.getGameState()) {
             // when a movement is going to send to the server, copy it for ten times so that the play can move faster
-            for (int i = 0; i < 10; i++) {
-                model.doCommand(new MovePlayerLeftCommand(ClientConfig.getInstance().getId()));
-            }
+//            for (int i = 0; i < 10; i++) {
+//                model.doCommand(new MovePlayerLeftCommand(ClientConfig.getInstance().getId()));
+//            }
+          model.doCommand(new MovePlayerLeftCommand(ClientConfig.getInstance().getId()));
         }
 
       } else {
@@ -234,9 +235,10 @@ public class GameController implements Controller {
       if (event.getKeyCode() == VK_RIGHT) {
         if (model.getGameState()) {
             // when a movement is going to send to the server, copy it for ten times so that the play can move faster
-            for (int i = 0; i < 10; i++) {
-                model.doCommand(new MovePlayerRightCommand(ClientConfig.getInstance().getId()));
-            }
+//            for (int i = 0; i < 10; i++) {
+//                model.doCommand(new MovePlayerRightCommand(ClientConfig.getInstance().getId()));
+//            }
+          model.doCommand(new MovePlayerRightCommand(ClientConfig.getInstance().getId()));
         }
       } else {
         if (nextChain != null) {
